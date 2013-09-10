da2cb28e5813fde5f18c8a51dcae7918fd6def26
aqingsao <vinci.zhang@gmail.com>
2013-09-10 18:33:43 +0800

    could print summary for links

--- link.rb
+++ link.rb
@@ -10,7 +10,26 @@ class Link
   end
   def Link.randomLink(nodes)
     index1 = rand(nodes.length);
-    index2 = rand(nodes.length);
+    index2 = rand(nodes.length) while index2.nil? || index2 == index1;
     Link.new(nodes[index1], nodes[index2])
+  end
+  def to_s
+    "Link(#{@node1.name}-#{@node2.name})"
+  end
+end
+
+class Links < Array
+  def print_summary
+    nodes_links = {}
+    self.each do |link|
+      nodes_links[link.node1] = [] if nodes_links[link.node1].nil?
+      nodes_links[link.node2] = [] if nodes_links[link.node2].nil?
+      nodes_links[link.node1] << link
+      nodes_links[link.node2] << link
+    end
+    p nodes_links.length
+    nodes_links.each_pair do |node, links|
+      p "#{node}: #{links.length}"
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,17 @@
+require File.join(File.dirname(__FILE__), "link.rb")
+
+link_factor = 1.5
 
 nodes = 100.times.each_with_object([]) do |i, nodes|
   nodes << Node.new(i.to_s)
 end
 
-links = 100.times.each_with_object([]) do |i, links|
-  
+
+links = (nodes.length * link_factor).to_i.times.each_with_object(Links.new) do |i, links|
+  link = Link.randomLink(nodes) while link.nil? || links.include?(link)
+  links << link
 end
+
+p "nodes: #{nodes.length}"
+p "links factor: #{link_factor}, links: #{links.length}"
+links.print_summary
